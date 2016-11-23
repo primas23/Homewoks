175f6f2d35935ef1d814fa333bcdd0b1904175ab
Petar Petrov <primas.petrov@gmail.com>
2016-11-23 20:07:45 +0200

    Added tasks 8 and 9

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/09.Sequnce/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.Sequnce
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter N:");
+            int n = int.Parse(Console.ReadLine());
+            int p = 50;
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(n);
+
+            Console.WriteLine("N = {0}", n);
+            Console.WriteLine("P = {0}", p);
+            Console.Write("S =");
+
+            var index = 0;
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                Console.Write(" {0}", current);
+                index++;
+
+                if (current == p)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+
+                queue.Enqueue(current + 1);
+                queue.Enqueue(2 * current);
+            }
+        }
+    }
+}
