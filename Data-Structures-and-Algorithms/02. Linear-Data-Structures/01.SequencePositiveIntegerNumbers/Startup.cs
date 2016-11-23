fdc6851015bc71bb86c48f6765c964ba92f5c921
Petar Petrov <primas.petrov@gmail.com>
2016-11-23 17:32:51 +0200

    Task 01 done

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/01.SequencePositiveIntegerNumbers/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.SequencePositiveIntegerNumbers
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter positive numbers. To break, enter blank line: ");
+
+            IList<int> sequence = new List<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == string.Empty)
+                {
+                    break;
+                }
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    throw new ArgumentNullException("The input must not be null!");
+                }
+
+                int number = int.Parse(input);
+                sequence.Add(number);
+            }
+
+            int sum = 0;
+            foreach (var item in sequence)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine("Sum: {0}, average: {1}", sum, (double)sum / sequence.Count);
+        }
+    }
+}
