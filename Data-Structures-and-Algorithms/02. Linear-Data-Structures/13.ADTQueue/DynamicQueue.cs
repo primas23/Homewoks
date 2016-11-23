84caa308defa032e18d5bfd7212cefbe3c95dc7f
Petar Petrov <primas.petrov@gmail.com>
2016-11-23 20:55:48 +0200

    Added Task 13

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/13.ADTQueue/DynamicQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _13.ADTQueue
+{
+    public class DynamicQueue<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        private int count;
+
+        public DynamicQueue()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        public void Enqueue(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+                tail = head;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = tail.Next;
+            }
+            count++;
+        }
+
+        public object Dequeue()
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var result = head.Data;
+            head = head.Next;
+            count--;
+            return head.Data;
+        }
+
+        public void Clear()
+        {
+            this.head = null;
+            this.tail = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        public T Peek()
+        {
+            return this.head.Data;
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/13.ADTQueue/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _13.ADTQueue
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            DynamicQueue<int> queue = new DynamicQueue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Count);
+            queue.Clear();
+            Console.WriteLine(queue.Count);
+        }
+    }
+}
