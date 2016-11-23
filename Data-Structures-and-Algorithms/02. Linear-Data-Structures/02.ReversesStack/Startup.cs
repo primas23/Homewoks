b4aa074e6e729db824dd1f669f62c347e9e9be2e
Petar Petrov <primas.petrov@gmail.com>
2016-11-23 17:44:57 +0200

    Task 02 done

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/02.ReversesStack/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.ReversesStack
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter positive numbers. To break, enter blank line: ");
+
+            Stack<int> stack = new Stack<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == string.Empty)
+                {
+                    break;
+                }
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    throw new ArgumentNullException("The input must not be null!");
+                }
+
+                int number = int.Parse(input);
+                stack.Push(number);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
