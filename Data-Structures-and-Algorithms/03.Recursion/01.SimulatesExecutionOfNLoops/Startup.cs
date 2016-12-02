bb7dc67c662f9c30315368786970500f346c21d6
Petar Petrov <primas.petrov@gmail.com>
2016-12-02 20:28:58 +0200

    Added

---
+++ Data-Structures-and-Algorithms/03.Recursion/01.SimulatesExecutionOfNLoops/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.SimulatesExecutionOfNLoops
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[number];
+
+            LoopMe(0, arr);
+        }
+
+        public static void LoopMe(int index, int[] arr)
+        {
+            if (index == arr.Length)
+            {
+                Console.WriteLine(String.Join("", arr));
+                return;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {                
+                arr[index] = i + 1;
+                LoopMe(index + 1, arr);
+            }            
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/03.Recursion/02.CombinationsWithDuplicates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02.CombinationsWithDuplicates
+{
+    public class Program
+    {
+        const int N = 3;
+        const int K = 2;
+
+        static int[] _arr = new int[K];
+
+        public static void Main()
+        {
+            GenerateCombinationsNoRepetitions(0, 0);
+        }
+
+        static void GenerateCombinationsNoRepetitions(int index, int start)
+        {
+            if (index >= K)
+            {
+                PrintVariations();
+            }
+            else
+            {
+                for (int i = start; i < N; i++)
+                {
+                    _arr[index] = i;
+                    GenerateCombinationsNoRepetitions(index + 1, i);
+                }
+            }
+        }
+
+        static void PrintVariations()
+        {
+            Console.Write("( ");
+            foreach (int t in _arr)
+            {
+                Console.Write(t + 1 + " ");
+            }
+            Console.WriteLine(')');
+        }
+    }
+}
