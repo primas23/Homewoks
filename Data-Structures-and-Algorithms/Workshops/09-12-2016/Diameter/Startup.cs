1e74d6a3597df8c890c01c979e6746ac1b8cd2ca
Petar Petrov <primas.petrov@gmail.com>
2016-12-15 12:44:40 +0200

    Added Diameter task

---
+++ Data-Structures-and-Algorithms/Workshops/09-12-2016/Diameter/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diameter
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
