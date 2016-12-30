bce9f5f3dfb898052794de58c690e0acb462177a
Petar Petrov <primas.petrov@gmail.com>
2016-12-30 18:08:43 +0200

    Bug fix "ng2-bootstrap": "^1.1.16",

--- Angular 2/top-movies/top-movies_skeleton/app/app.module.ts
+++ Angular 2/top-movies/top-movies_skeleton/app/app.module.ts
@@ -4,9 +4,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-import {Ng2BootstrapModule} from 'ng2-bootstrap/ng2-bootstrap';
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
-    // put all the needed data here
+    imports: [BrowserModule,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
